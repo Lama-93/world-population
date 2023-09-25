724fe8211d74b99d3aaa02cc5091920d1201df98
Lama-93 <146031962+Lama-93@users.noreply.github.com>
2023-09-25 23:53:11 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,10 +6,6 @@ import streamlit as st
 st.title("Interactive Data Visualizations with World Population for year 2023")
 st.write("Explore an interactive bar chart with population data.")
 
-# Load and display the HTML visualization
-#with open('/Users/lamaissa/Downloads/interactive_bar_chart.html', 'r', encoding='utf-8') as f:
- #   html = f.read()
-#st.components.v1.html(html, width=800, height=600)  # Adjust width and height as needed
 # Load and display the HTML visualization using a relative path
 with open('interactive_bar_chart.html', 'r', encoding='utf-8') as f:
     html = f.read()
@@ -18,26 +14,23 @@ st.components.v1.html(html, width=800, height=600)  # Adjust width and height as
 # 2 Set the title and introductory text for your app
 st.write("Explore an interactive scatter plot with population data.")
 
-# Load and display the HTML visualization
-with open('/Users/lamaissa/Downloads/3d_scatter_plot5.html', 'r', encoding='utf-8') as f:
+# Load and display the HTML visualization using a relative path
+with open('3d_scatter_plot5.html', 'r', encoding='utf-8') as f:
     html = f.read()
 st.components.v1.html(html, width=800, height=600)  # Adjust width and height as needed
-
 
 # 3 Set the title and introductory text for your app
 st.write("Explore an interactive World map with population data.")
 
-# Load and display the HTML visualization
-with open('/Users/lamaissa/Downloads/interactive_choropleth_map.html', 'r', encoding='utf-8') as f:
+# Load and display the HTML visualization using a relative path
+with open('interactive_choropleth_map.html', 'r', encoding='utf-8') as f:
     html = f.read()
 st.components.v1.html(html, width=800, height=600)  # Adjust width and height as needed
-
-
 
 # 4 Set the title and introductory text for your app
 st.write("Explore an interactive Jupiter table with population data.")
 
-# Load and display the HTML visualization
-with open('/Users/lamaissa/Downloads/jupyter-table1.html', 'r', encoding='utf-8') as f:
+# Load and display the HTML visualization using a relative path
+with open('jupyter-table1.html', 'r', encoding='utf-8') as f:
     html = f.read()
-st.components.v1.html(html, width=800, height=600)  # Adjust width and height as n
+st.components.v1.html(html, width=800, height=600)  # Adjust width and height as needed
