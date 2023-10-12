a5979d8416cb13a4e6bbd15806fbf4f135aa35f8
Lama-93 <146031962+Lama-93@users.noreply.github.com>
2023-10-13 00:51:12 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,3 @@
-
-
 import streamlit as st
 import pandas as pd
 import plotly.express as px
@@ -9,7 +7,7 @@ import numpy as np
 # Load the dataset
 @st.cache
 def load_data():
-    data = pd.read_csv("WorldPopulation2023.csv")
+    data = pd read_csv("WorldPopulation2023.csv")
     return data
 
 data = load_data()
@@ -20,33 +18,31 @@ st.title("World Population 2023 Data Visualization")
 # Display the dataset (optional)
 st.dataframe(data)
 
-# Create a filter to select a random set of 3 countries for comparison
-#default_countries = random.sample(data["Country"].unique().tolist(), 3)
-#selected_countries = st.sidebar.multiselect("Select Countries for Comparison", data["Country"].unique(), default=default_countries)
-
 # Create a multi-select widget to select multiple countries
 selected_countries = st.sidebar.multiselect("Select Countries for Comparison", data["Country"].unique())
 
+# Display a title and description for the bar chart
+st.write("## Population Comparison")
+st.write("This bar chart shows the population of the selected countries for comparison.")
 # Filter the data based on the selected countries
 filtered_data = data[data["Country"].isin(selected_countries)]
 
 # Display a bar chart for the selected countries' population
 if not filtered_data.empty:
-    st.write("## Population for Selected Countries")
     fig_bar = px.bar(filtered_data, x="Country", y="Population2023", title="Population Comparison")
     st.plotly_chart(fig_bar)
 else:
     st.write("No data selected. Please choose one or more countries for comparison.")
 
-
-
-
 # Get unique median age values from the dataset
 median_age_values = np.sort(data["MedianAge"].unique())
 
 # Create a slider for selecting median age
-selected_median_age = st.slider("Select Median Age", min_value=int(min(median_age_values)), max_value=int(max(median_age_values)), value=int(min(median_age_values)))
+selected_median_age = st.slider("Select Median Age", min_value=int(min(median_age_values)), max_value=int(max(median_age_values)), value=int(min(median_age_values))
 
+# Display a title and description for the choropleth map
+st.write("## Median Age Choropleth Map")
+st.write("This choropleth map displays the median age of countries with colors indicating the median age.")
 # Round the selected median age to the nearest integer
 selected_median_age = int(selected_median_age)
 
@@ -54,7 +50,6 @@ selected_median_age = int(selected_median_age)
 filtered_data = data[data["MedianAge"] == selected_median_age]
 
 # Create a choropleth map with median age data
-st.write("## World Population Choropleth Map by Median Age")
 fig_map = px.choropleth(
     filtered_data,
     locations="Country",
