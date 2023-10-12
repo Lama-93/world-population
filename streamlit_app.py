cfacf2b2537e0963f90cd7b7af55f53e1dcb9dd0
Lama-93 <146031962+Lama-93@users.noreply.github.com>
2023-10-13 00:20:58 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,13 +16,16 @@ st.title("World Population 2023 Data Visualization")
 # Display the dataset (optional)
 st.dataframe(data)
 
-# Create a filter to select a country
-selected_country = st.sidebar.selectbox("Select a Country", data["Country"].unique())
+# Create a multi-select widget to select multiple countries
+selected_countries = st.sidebar.multiselect("Select Countries for Comparison", data["Country"].unique())
 
-# Filter the data based on the selected country
-filtered_data = data[data["Country"] == selected_country]
+# Filter the data based on the selected countries
+filtered_data = data[data["Country"].isin(selected_countries)]
 
-# Display a bar chart for the selected country's population
-st.write("## Population for Selected Country")
-fig_bar = px.bar(filtered_data, x="Country", y="Population2023", title=f"Population for {selected_country}")
-st.plotly_chart(fig_bar)
+# Display a bar chart for the selected countries' population
+if not filtered_data.empty:
+    st.write("## Population for Selected Countries")
+    fig_bar = px.bar(filtered_data, x="Country", y="Population2023", title="Population Comparison")
+    st.plotly_chart(fig_bar)
+else:
+    st.write("No data selected. Please choose one or more countries for comparison.")
