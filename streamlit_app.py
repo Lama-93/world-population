5d4ab95482bc7d9aa33015f425547890857f041f
Lama-93 <146031962+Lama-93@users.noreply.github.com>
2023-10-13 00:29:19 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -31,3 +31,35 @@ if not filtered_data.empty:
     st.plotly_chart(fig_bar)
 else:
     st.write("No data selected. Please choose one or more countries for comparison.")
+
+# Create a slider for selecting median age
+selected_median_age = st.slider("Select Median Age", min_value=data["MedianAge"].min(), max_value=data["MedianAge"].max(), value=data["MedianAge"].min())
+
+# Filter the data based on the selected median age
+filtered_data = data[data["MedianAge"] == selected_median_age]
+
+# Create a world map with population data and color by median age
+st.write("## World Population Map by Median Age")
+fig_map = px.scatter_geo(
+    filtered_data,
+    locations="Country",
+    locationmode="country names",
+    color="MedianAge",
+    size="Population2023",
+    title=f"World Population with Median Age {selected_median_age}",
+    hover_name="Country",
+    projection="natural earth",
+    color_continuous_scale=px.colors.sequential.Plasma,
+)
+
+fig_map.update_geos(
+    showcoastlines=True,
+    coastlinecolor="Black",
+)
+
+st.plotly_chart(fig_map)
+
+
+
+
+
