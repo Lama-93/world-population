a3324b44863c29aa355e170f2d23712c682209a3
Lama-93 <146031962+Lama-93@users.noreply.github.com>
2023-10-12 23:04:23 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,7 @@
-import pandas as pd
-import numpy as np
 import streamlit as st
+import pandas as pd
 import plotly.express as px
-import plotly.graph_objects as go
+
 # Load the dataset from GitHub
 @st.cache
 def load_data():
@@ -18,24 +17,23 @@ st.title("Population Data Analysis for 2023")
 st.write("Explore population data for the year 2023.")
 
 # Create a slider for population
-min_population = st.slider("Minimum Population", int(data["Population2023"].min()), int(data["Population2023"].max()), int(data["Population2023"].min()))
-max_population = st.slider("Maximum Population", min_population, int(data["Population2023"].max()), int(data["Population2023"].max()))
+selected_population = st.slider("Select a Population Range", int(data["Population2023"].min()), int(data["Population2023"].max()), (int(data["Population2023"].min()), int(data["Population2023"].max())))
 
 # Filter the data based on the selected population range
-filtered_data = data[(data["Population2023"] >= min_population) & (data["Population2023"] <= max_population)]
+filtered_data = data[(data["Population2023"] >= selected_population[0]) & (data["Population2023"] <= selected_population[1])]
 
-# Create a bar chart based on the filtered data
-st.subheader("Population by Country")
-fig = px.bar(
+# Create a map based on the filtered data
+st.subheader("Map of Countries by Population")
+fig = px.scatter_geo(
     filtered_data,
-    x="Country",
-    y="Population2023",
-    title=f"Population by Country in 2023",
-    labels={'Population2023': 'Population'},
+    locations="Country",
+    locationmode="country names",
+    text="Country",
+    size="Population2023",
+    projection="natural earth",
 )
 
-fig.update_xaxes(title_text="Country")
-fig.update_yaxes(title_text="Population")
+fig.update_geos(showcoastlines=True, coastlinecolor="Black", showland=True, landcolor="white")
 
-# Display the bar chart
+# Display the map
 st.plotly_chart(fig)
