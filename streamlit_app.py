dfcdd2529da00b9fff1b122481d03e983703039f
Lama-93 <146031962+Lama-93@users.noreply.github.com>
2023-10-12 22:55:34 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,3 +43,16 @@ st.write(filtered_data[[selected_column]])
 st.subheader("Visualization of Filtered Data")
 fig = px.bar(filtered_data, x="Country", y=selected_column, title=f"{selected_column} for {selected_country}")
 st.plotly_chart(fig)
+# Create a slider for population
+min_population = st.slider("Minimum Population", int(data["Population2023"].min()), int(data["Population2023"].max()), int(data["Population2023"].min()))
+max_population = st.slider("Maximum Population", min_population, int(data["Population2023"].max()), int(data["Population2023"].max()))
+
+# Filter the data based on the selected population range
+filtered_data = data[(data["Population2023"] >= min_population) & (data["Population2023"] <= max_population)]
+
+# Create a map based on the filtered data
+st.subheader("Map of Countries by Population")
+fig = px.scatter_geo(filtered_data, locations="Country", locationmode="country names", text="Country", size="Population2023", projection="natural earth")
+fig.update_geos(showcoastlines=True, coastlinecolor="Black")
+fig.update_layout(geo=dict(showland=True, landcolor="rgb(217, 217, 217)"))
+st.plotly_chart(fig)
