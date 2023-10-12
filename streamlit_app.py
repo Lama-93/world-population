316f34946bf060ed315c39cd71093555c22fb01c
Lama-93 <146031962+Lama-93@users.noreply.github.com>
2023-10-12 22:56:32 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,36 +13,8 @@ data = load_data()
 
 # Set the title and introductory text for your app
 st.title("Interactive World Population Data for 2023")
-st.write("Explore and filter world population data for the year 2023.")
+st.write("Explore world population data for the year 2023 on a map.")
 
-# Create a sidebar with filters
-st.sidebar.header("Filter Data")
-
-# Sort Option
-sort_by_population = st.sidebar.checkbox("Sort by Population")
-
-# Filter by Country
-selected_country = st.sidebar.selectbox("Select a Country:", data["Country"].unique())
-
-# Filter by Columns
-selected_column = st.sidebar.selectbox("Select a Column:", data.columns)
-
-# Filter and Sort the data based on user selections
-if sort_by_population:
-    filtered_data = data.sort_values(by="Population2023", ascending=False)
-else:
-    filtered_data = data
-
-filtered_data = filtered_data[filtered_data["Country"] == selected_country]
-
-# Display the filtered data
-st.write(f"Population data for {selected_country} based on {selected_column}:")
-st.write(filtered_data[[selected_column]])
-
-# Create a bar chart based on the filtered data
-st.subheader("Visualization of Filtered Data")
-fig = px.bar(filtered_data, x="Country", y=selected_column, title=f"{selected_column} for {selected_country}")
-st.plotly_chart(fig)
 # Create a slider for population
 min_population = st.slider("Minimum Population", int(data["Population2023"].min()), int(data["Population2023"].max()), int(data["Population2023"].min()))
 max_population = st.slider("Maximum Population", min_population, int(data["Population2023"].max()), int(data["Population2023"].max()))
