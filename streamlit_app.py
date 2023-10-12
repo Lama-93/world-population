f44a3814962d8410033ca0346665c556b8e5528b
Lama-93 <146031962+Lama-93@users.noreply.github.com>
2023-10-12 23:18:52 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,31 +11,29 @@ def load_data():
 
 # Load the data
 data = load_data()
+# Create a Streamlit app
+st.title('World Population 2023 Map')
+
 # Add a slider for selecting the population range
-population_slider = st.slider('Select Population Range', min_value=WorldPopulation2023['Population2023'].min(), max_value=WorldPopulation2023['Population2023'].max(), value=(0, WorldPopulation2023['Population2023'].max()))
+population_slider = st.slider('Select Population Range', min_value=0, max_value=world_population_data['Population2023'].max(), value=(0, world_population_data['Population2023'].max()))
 
 # Filter the data based on the selected population range
-filtered_data = WorldPopulation2023[(WorldPopulation2023['Population2023'] >= population_slider[0]) & (WorldPopulation2023['population'] <= population_slider[1])]
+filtered_data = world_population_data[(world_population_data['Population2023'] >= population_slider[0]) & (world_population_data['Population2023'] <= population_slider[1])]
 
-# Create the suicide rate map for the selected year
-def create_suicide_rate_map(filtered_data):
-    # Create the choropleth map
-    fig_map = px.choropleth(
-        filtered_data,
-        locations="country",
-        locationmode='country names',
-        color="Population2023",
-        hover_name="country",
-        projection="natural earth",
-        title="Population Rate by Country",
-        color_continuous_scale=px.colors.sequential.Viridis,
-        labels={'WorldPopulation2023': 'Population2023'},
-    )
+# Create a choropleth map
+fig_map = px.choropleth(
+    filtered_data,
+    locations="Country",  # Replace with the appropriate column name from your dataset
+    locationmode='country names',
+    color="Population2023",  # Replace with the appropriate column name from your dataset
+    hover_name="Country",  # Replace with the appropriate column name from your dataset
+    projection="natural earth",
+    title="World Population 2023 by Country",
+    color_continuous_scale=px.colors.sequential.Viridis,
+    labels={'Population2023': 'Population 2023'},
+)
 
-    fig_map.update_geos(showcoastlines=True, coastlinecolor="Black", showland=True, landcolor="white")
-
-    return fig_map
+fig_map.update_geos(showcoastlines=True, coastlinecolor="Black", showland=True, landcolor="white")
 
 # Display the map in the Streamlit app
-fig = create_suicide_rate_map(filtered_data)
-st.plotly_chart(fig)
+st.plotly_chart(fig_map)
