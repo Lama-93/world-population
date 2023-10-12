949486ea83da8325e8ae99c62cf9f9e885f4aa6c
Lama-93 <146031962+Lama-93@users.noreply.github.com>
2023-10-13 01:18:05 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,8 +29,9 @@ selected_countries = st.sidebar.multiselect("Select Countries for Comparison", d
 # Filter the data based on the selected countries
 filtered_data = data[data["Country"].isin(selected_countries)]
 
-st.title("Bar Cart for selected countries'population in 2023")
+
 # Display a bar chart for the selected countries' population
+st.title("Bar Cart for selected countries'population in 2023")
 if not filtered_data.empty:
     st.write("## Population for Selected Countries")
     fig_bar = px.bar(filtered_data, x="Country", y="Population2023", title="Population Comparison")
