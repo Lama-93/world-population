bac05a3b0a9aeb778626adb2cff318afbde9c80b
Lama-93 <146031962+Lama-93@users.noreply.github.com>
2023-10-13 00:52:11 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,7 +7,7 @@ import numpy as np
 # Load the dataset
 @st.cache
 def load_data():
-    data = pd read_csv("WorldPopulation2023.csv")
+    data = pd.read_csv("WorldPopulation2023.csv")
     return data
 
 data = load_data()
