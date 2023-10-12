e496c14753872f8aa4fdeaceabfde571067cf42a
Lama-93 <146031962+Lama-93@users.noreply.github.com>
2023-10-12 23:32:48 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,20 +1,10 @@
-import streamlit as st
 import pandas as pd
-#!pip install plotly
-install pip
-pip install -r requirements.txt
+import streamlit as st
+import folium
 
-import plotly.express as px
+# Load your "WorldPopulation2023" dataset
+world_population_data = pd.read_csv('WorldPopulation2023.csv')
 
-# Load the dataset from GitHub
-@st.cache
-def load_data():
-    url = 'https://raw.githubusercontent.com/Lama-93/world-population/main/WorldPopulation2023.csv'
-    data = pd.read_csv(url)
-    return data
-
-# Load the data
-data = load_data()
 # Create a Streamlit app
 st.title('World Population 2023 Map')
 
@@ -24,20 +14,12 @@ population_slider = st.slider('Select Population Range', min_value=0, max_value=
 # Filter the data based on the selected population range
 filtered_data = world_population_data[(world_population_data['Population2023'] >= population_slider[0]) & (world_population_data['Population2023'] <= population_slider[1])]
 
-# Create a choropleth map
-fig_map = px.choropleth(
-    filtered_data,
-    locations="Country",  # Replace with the appropriate column name from your dataset
-    locationmode='country names',
-    color="Population2023",  # Replace with the appropriate column name from your dataset
-    hover_name="Country",  # Replace with the appropriate column name from your dataset
-    projection="natural earth",
-    title="World Population 2023 by Country",
-    color_continuous_scale=px.colors.sequential.Viridis,
-    labels={'Population2023': 'Population 2023'},
-)
+# Create a folium map
+m = folium.Map(location=[0, 0], zoom_start=2)
 
-fig_map.update_geos(showcoastlines=True, coastlinecolor="Black", showland=True, landcolor="white")
+# Add markers for each country
+for _, row in filtered_data.iterrows():
+    folium.Marker([row['Latitude'], row['Longitude']], popup=f"{row['Country']} - {row['Population2023']}").add_to(m)
 
 # Display the map in the Streamlit app
-st.plotly_chart(fig_map)
+st.markdown(m._repr_html_(), unsafe_allow_html=True)
